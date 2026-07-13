2b745d1f56ef56a37dd853d67e09989cc06e6c22
Miguel Torrez <108190997+migueltorrezd@users.noreply.github.com>
2026-07-13 10:26:13 +0200

    fix: update URLs for the migueltorrezd account rename

--- Casks/yappyapp.rb
+++ Casks/yappyapp.rb
@@ -2,7 +2,7 @@ cask "yappyapp" do
   version "0.3.1"
   sha256 "183902875c68b7c6fb629a68693600911392bb83bf4a28154260e3d84402ae9c"
 
-  url "https://github.com/matd97/yappyapp-releases/releases/download/v#{version}/YappYapp-#{version}.dmg"
+  url "https://github.com/migueltorrezd/yappyapp-releases/releases/download/v#{version}/YappYapp-#{version}.dmg"
   name "YappYapp"
   desc "Local-only Mac dictation app with on-device Whisper and Parakeet transcription"
   homepage "https://yappyapp.app"
